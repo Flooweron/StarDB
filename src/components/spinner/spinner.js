09ee4e1f49950fde92e75649786be4971d305669
Flooweron <63611265+Flooweron@users.noreply.github.com>
2021-02-17 12:26:24 +0400

    not ready

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './spinner.css'
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-double-ring-tx7ud6twsup">
+            <div className="ldio-wilveo84i4h">
+                <div></div>
+                <div></div>
+                    <div><div></div></div>
+                    <div><div></div></div>
+            </div>
+        </div>
+    )
+}
+export default Spinner
